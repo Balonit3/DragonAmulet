8b7c9de586890c7fedffd6e6dd5310a9c8e1b9aa
darkkrogonk <darkkrogonk@gmail.com>
2024-04-04 18:20:06 +0300

    сделал оформление

--- hero.py
+++ hero.py
@@ -1,4 +1,6 @@
 from time import sleep
+
+
 def bordered(text):
     lines = text.splitlines()
     width = max(len(s) for s in lines)
@@ -7,74 +9,85 @@ def bordered(text):
         res.append('│' + (s + ' ' * width)[:width] + '│')
     res.append('└' + '─' * width + '┘')
     return '\n'.join(res)
+
+
 class Hero():
-    def __init__(self,name,health,armor,power,weapon):
+    def __init__(self, name, health, armor, power, weapon):
         self.name = name
         self.health = health
         self.armor = armor
         self.power = power
         self.weapon = weapon
+
     def print_info(self):
-        print('Поприветсвтуйте героя ->',self.name)
+        print('Поприветсвтуйте героя ->', self.name)
         sleep(1)
-        print('Уровень здоровья:',self.health)
+        print('Уровень здоровья:', self.health)
         sleep(1)
-        print('Класс брони:',self.armor)
+        print('Класс брони:', self.armor)
         sleep(1)
-        print('Сила удара:',self.power)
+        print('Сила удара:', self.power)
         sleep(1)
-        print('Оружие:',self.weapon)
+        print('Оружие:', self.weapon)
         sleep(1)
         print(' ')
-    def strike(self,enemy):
-        print('Удар',self.name,'атакует',enemy.name,'силой',self.power,'используя',self.weapon,'\n')
+
+    def strike(self, enemy):
+        print('Удар', self.name, 'атакует', enemy.name, 'силой', self.power, 'используя', self.weapon, '\n')
         enemy.armor -= self.power
         if enemy.armor < 0:
             enemy.health += enemy.armor
             enemy.armor = 0
-    def strike_gnom(self,enemy_gnom):
-        print('Удар',self.name,'атакует',enemy_gnom.name,'силой',self.power,'используя',self.weapon,'\n')
+
+    def strike_gnom(self, enemy_gnom):
+        print('Удар', self.name, 'атакует', enemy_gnom.name, 'силой', self.power, 'используя', self.weapon, '\n')
         enemy_gnom.armor -= self.power
         if enemy_gnom.armor < 0:
             enemy_gnom.health += enemy_gnom.armor
             enemy_gnom.armor = 0
-        print(enemy_gnom.name,'получил удар\nкласс брони упал',enemy_gnom.armor,'Уровень здоровья упал',enemy_gnom.health,'\n')
-    def fight(self,enemy):
+        print(enemy_gnom.name, 'получил удар\nкласс брони упал', enemy_gnom.armor, 'Уровень здоровья упал',
+              enemy_gnom.health, '\n')
+
+    def fight(self, enemy):
         while self.health > 0 and enemy.health > 0:
             self.strike(enemy)
             if enemy.health <= 0:
-                print(enemy.name,'пал в бою!!!!\n')
+                print(enemy.name, 'пал в бою!!!!\n')
                 break
             sleep(5)
             enemy.strike(self)
             if self.health <= 0:
-                print(self.name,'пал в бою!!!!\n')
+                print(self.name, 'пал в бою!!!!\n')
                 break
             sleep(5)
-    def fight_Jo(self,enemy_JO):
+
+    def fight_Jo(self, enemy_JO):
         while self.health > 0 and enemy_JO.health > 0:
             self.strike(enemy_JO)
             if enemy_JO.health <= 0:
-                print(enemy_JO.name,'пал в бою!!!!\n')
+                print(enemy_JO.name, 'пал в бою!!!!\n')
                 break
             sleep(5)
             enemy_JO.strike(self)
             if self.health <= 0:
-                print(self.name,'пал в бою!!!!\n')
+                print(self.name, 'пал в бою!!!!\n')
                 break
             sleep(5)
-    def fight_viper(self,enemy_kislota):
+
+    def fight_viper(self, enemy_kislota):
         while self.health > 0 and enemy_kislota.health > 0:
             self.strike(enemy_kislota)
             if enemy_kislota.health <= 0:
-                print(enemy_kislota.name,'пал в бою!!!!\n')
+                print(enemy_kislota.name, 'пал в бою!!!!\n')
                 break
             sleep(5)
             enemy_kislota.strike(self)
             if self.health <= 0:
-                print(self.name,'пал в бою!!!!\n')
+                print(self.name, 'пал в бою!!!!\n')
                 break
             sleep(5)
+
+
 def start_history():
     print(bordered('история Ричарда началась когда его отец не мало известный рыцарь погиб от лап дракона,'))
     sleep(2)
@@ -84,13 +97,17 @@ def start_history():
     sleep(2)
     print(bordered('И вот с той трагедии прошло 12 лет и Ричард решил,что время мести настало.'))
     sleep(2)
-    print(bordered('Ричард двинулся в путь и пройдя через реки и поля он наконец то подходит к следующему припядствию. '))
+    print(
+        bordered('Ричард двинулся в путь и пройдя через реки и поля он наконец то подходит к следующему припядствию. '))
     sleep(2)
-    print(bordered('Это был тёмный и мрачный лес.Ричарду было страшно в него заходить,но он вспомнил про свою цель и не задумываясь вступил в него.'))
+    print(bordered(
+        'Это был тёмный и мрачный лес.Ричарду было страшно в него заходить,но он вспомнил про свою цель и не задумываясь вступил в него.'))
     sleep(2)
-    print(bordered('И вот пройдя уже по леса он подумал, что дальше будет также легко,он даже удивился почему этот лес все так боялись.'))
+    print(bordered(
+        'И вот пройдя уже по леса он подумал, что дальше будет также легко,он даже удивился почему этот лес все так боялись.'))
     sleep(2)
-    print(bordered('Но вдруг из гущи леса на него выбегают 3 гнома которые требуют с него 3 золотые монеты,либо они его убьют.'))
+    print(bordered(
+        'Но вдруг из гущи леса на него выбегают 3 гнома которые требуют с него 3 золотые монеты,либо они его убьют.'))
     sleep(2)
     print(bordered('Он не растерялся и с помощью своего меча отрубил 3 головы сразу.'))
     sleep(2)
@@ -102,7 +119,8 @@ def start_history():
     sleep(2)
     print(bordered('После этой схватки у Ричардо появился новый предмет как ПИВО,'))
     sleep(2)
-    print(bordered('это было зелье исцеление которое он мог выпить в любой следующей битве что бы восполнить своё здоровье '))
+    print(bordered(
+        'это было зелье исцеление которое он мог выпить в любой следующей битве что бы восполнить своё здоровье '))
     sleep(2)
     print(bordered('Позже он двинулся на север в сторону города под названием АНОР ЛОНДО.'))
     sleep(2)
@@ -116,13 +134,17 @@ def start_history():
     sleep(2)
     print(bordered('Но для ричарда его слова были лишь мотивацией пойти туда.'))
     sleep(2)
-    print(bordered('И вот спустя 3 дня непрерывной дороги он видит ущелье в котором очень странно светится ярко-зелёным цветом река и скалы.'))
+    print(bordered(
+        'И вот спустя 3 дня непрерывной дороги он видит ущелье в котором очень странно светится ярко-зелёным цветом река и скалы.'))
     sleep(2)
-    print(bordered('Пройдя немного дальше он слышит громкий топот и сопение и повернув голову влево он видит его,того самого дракона про которого говорил кузнец.'))
+    print(bordered(
+        'Пройдя немного дальше он слышит громкий топот и сопение и повернув голову влево он видит его,того самого дракона про которого говорил кузнец.'))
     sleep(2)
-    print(bordered('Весь покрыт плотной чешуёй тёмно-зелёного цвета дракон идёт в сторону Ричарда и Ричард понимает что битвы не избежать.'))
+    print(bordered(
+        'Весь покрыт плотной чешуёй тёмно-зелёного цвета дракон идёт в сторону Ричарда и Ричард понимает что битвы не избежать.'))
     sleep(2)
-    print(bordered('И ричард делает первый рывок в его сторону и наносит своим мечом первый удар в зону живота,но из за плотной чешуи дракон не чувствует удара и атакует в ответ.'))
+    print(bordered(
+        'И ричард делает первый рывок в его сторону и наносит своим мечом первый удар в зону живота,но из за плотной чешуи дракон не чувствует удара и атакует в ответ.'))
     sleep(2)
     print(bordered('выпустив свой яд, небольшая часть одежды Ричарда расщепляется.\n '))
     sleep(2)
