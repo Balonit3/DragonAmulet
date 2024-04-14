6817772cfd0042573778d0ab26e8a36c3ff9eced
darkkrogonk <darkkrogonk@gmail.com>
2024-04-14 15:21:55 +0300

    Разбивка по классам Тестирование новых персонажей и их способностей

--- hero.py
+++ hero.py
@@ -12,7 +12,7 @@ def bordered(text, color_code):
     return '\n'.join(res)
 
 
-class Hero():
+class Hero:
     def __init__(self, name, health, armor, power, weapon):
         self.name = name
         self.health = health
@@ -40,15 +40,16 @@ class Hero():
             enemy.health += enemy.armor
             enemy.armor = 0
 
-    def strike_gnom(self, enemy_gnom):
-        print('Удар', self.name, 'атакует', enemy_gnom.name, 'силой', self.power, 'используя', self.weapon, '\n')
-        enemy_gnom.armor -= self.power
-        if enemy_gnom.armor < 0:
-            enemy_gnom.health += enemy_gnom.armor
-            enemy_gnom.armor = 0
-        print(enemy_gnom.name, 'получил удар\nкласс брони упал', enemy_gnom.armor, 'Уровень здоровья упал',
-              enemy_gnom.health, '\n')
-
+    #
+    # def strike_gnom(self, enemy_gnom):
+    #     print('Удар', self.name, 'атакует', enemy_gnom.name, 'силой', self.power, 'используя', self.weapon, '\n')
+    #     enemy_gnom.armor -= self.power
+    #     if enemy_gnom.armor < 0:
+    #         enemy_gnom.health += enemy_gnom.armor
+    #         enemy_gnom.armor = 0
+    #     print(enemy_gnom.name, 'получил удар\nкласс брони упал', enemy_gnom.armor, 'Уровень здоровья упал',
+    #           enemy_gnom.health, '\n')
+    #
     def fight(self, enemy):
         while self.health > 0 and enemy.health > 0:
             self.strike(enemy)
@@ -61,32 +62,77 @@ class Hero():
                 print(self.name, 'пал в бою!!!!\n')
                 break
             sleep(5)
+    #
+    # def fight_Jo(self, enemy_JO):
+    #     while self.health > 0 and enemy_JO.health > 0:
+    #         self.strike(enemy_JO)
+    #         if enemy_JO.health <= 0:
+    #             print(enemy_JO.name, 'пал в бою!!!!\n')
+    #             break
+    #         sleep(5)
+    #         enemy_JO.strike(self)
+    #         if self.health <= 0:
+    #             print(self.name, 'пал в бою!!!!\n')
+    #             break
+    #         sleep(5)
+    #
+    # def fight_viper(self, enemy_kislota):
+    #     while self.health > 0 and enemy_kislota.health > 0:
+    #         self.strike(enemy_kislota)
+    #         if enemy_kislota.health <= 0:
+    #             print(enemy_kislota.name, 'пал в бою!!!!\n')
+    #             break
+    #         sleep(5)
+    #         enemy_kislota.strike(self)
+    #         if self.health <= 0:
+    #             print(self.name, 'пал в бою!!!!\n')
+    #             break
+    #         sleep(5)
 
-    def fight_Jo(self, enemy_JO):
-        while self.health > 0 and enemy_JO.health > 0:
-            self.strike(enemy_JO)
-            if enemy_JO.health <= 0:
-                print(enemy_JO.name, 'пал в бою!!!!\n')
-                break
-            sleep(5)
-            enemy_JO.strike(self)
-            if self.health <= 0:
-                print(self.name, 'пал в бою!!!!\n')
-                break
-            sleep(5)
 
-    def fight_viper(self, enemy_kislota):
-        while self.health > 0 and enemy_kislota.health > 0:
-            self.strike(enemy_kislota)
-            if enemy_kislota.health <= 0:
-                print(enemy_kislota.name, 'пал в бою!!!!\n')
-                break
-            sleep(5)
-            enemy_kislota.strike(self)
-            if self.health <= 0:
-                print(self.name, 'пал в бою!!!!\n')
-                break
-            sleep(5)
+class NPC:
+    def __init__(self, name, add_skill):
+        self.name = name
+        self.add_skill = add_skill
+
+    def health(self, hero):
+        hero.health += 10
+        print(self.name, 'Исцелил нашего героя', hero.name)
+        print('Теперь уровень здоровья героя - ', hero.health)
+
+    def repair(self, hero):
+
+        if hero.armor <= 100:
+            hero.armor = 100
+
+        print(self.name, 'Починил броню героя, теперь урвень брони составляет', hero.armor)
+
+
+class Enemy(Hero):
+    def __init__(self, name, health, armor, power, weapon, add_skill):
+        super().__init__(name, health, armor, power, weapon)
+        self.add_skill = add_skill
+
+    def apply_skill(self):
+        print(f'Враг {self.name} применил уникальный навык {self.add_skill} теперь он стал сильнее')
+
+
+class Gost(Enemy):
+    def __init__(self, name, health, armor, power, weapon, add_skill, visibility=True):
+        super().__init__(name, health, armor, power, weapon, add_skill)
+        self.visibility = visibility
+
+    def invisible(self):
+        print(f'{self.name} Скрылся с глаз героя, его теперь нельзя атаковать')
+        self.visibility = False
+
+
+# 1 - Главный герой
+# 2 - Враг
+# 2.1 - Дракон
+# 2.2 - Монстры
+# 2.3 - Нежить
+# 3 - NPS
 
 
 # Коды для цвета рамки
@@ -94,39 +140,50 @@ color_code_red = "91"
 color_code_blue = "94"
 color_code_green = "92"
 color_code_yellow = "93"
+
+
 def start_history():
-    print(bordered('история Ричарда началась когда его отец не мало известный рыцарь погиб от лап дракона', color_code_green))
+    print(bordered('история Ричарда началась когда его отец не мало известный рыцарь погиб от лап дракона',
+                   color_code_green))
     sleep(2)
     print(bordered('Ричарду на тот момент было 5 лет и с тех пор он решил,что он должен отомстить.', color_code_green))
     sleep(2)
-    print(bordered('И вот шли годы на протяжении всего этого времени он тренировался не покладая рук.', color_code_green))
+    print(
+        bordered('И вот шли годы на протяжении всего этого времени он тренировался не покладая рук.', color_code_green))
     sleep(2)
     print(bordered('И вот с той трагедии прошло 12 лет и Ричард решил,что время мести настало.', color_code_green))
     sleep(2)
     print(
-        bordered('Ричард двинулся в путь и пройдя через реки и поля он наконец то подходит к следующему припядствию. ', color_code_green))
+        bordered('Ричард двинулся в путь и пройдя через реки и поля он наконец то подходит к следующему припядствию. ',
+                 color_code_green))
     sleep(2)
     print(bordered(
-        'Это был тёмный и мрачный лес.Ричарду было страшно в него заходить,но он вспомнил про свою цель и не задумываясь вступил в него.', color_code_green))
+        'Это был тёмный и мрачный лес.Ричарду было страшно в него заходить,но он вспомнил про свою цель и не задумываясь вступил в него.',
+        color_code_green))
     sleep(2)
     print(bordered(
-        'И вот пройдя уже по леса он подумал, что дальше будет также легко,он даже удивился почему этот лес все так боялись.', color_code_green))
+        'И вот пройдя уже по леса он подумал, что дальше будет также легко,он даже удивился почему этот лес все так боялись.',
+        color_code_green))
     sleep(2)
     print(bordered(
-        'Но вдруг из гущи леса на него выбегают 3 гнома которые требуют с него 3 золотые монеты,либо они его убьют.', color_code_green))
+        'Но вдруг из гущи леса на него выбегают 3 гнома которые требуют с него 3 золотые монеты,либо они его убьют.',
+        color_code_green))
     sleep(2)
     print(bordered('Он не растерялся и с помощью своего меча отрубил 3 головы сразу.', color_code_green))
     sleep(2)
     print(bordered('После этой непростой битвы он решил расслабиться и пойти в бар к кинтавру Джо.', color_code_green))
     sleep(2)
-    print(bordered('Но по скольку этот лес не простой вместо желанной бутылочки пивчанского он получил суровый бой.', color_code_green))
+    print(bordered('Но по скольку этот лес не простой вместо желанной бутылочки пивчанского он получил суровый бой.',
+                   color_code_green))
     sleep(2)
-    print(bordered('Джо не обладал сверх навыками но был готов дать отпор Ричарду за свой божественный напиток ', color_code_green))
+    print(bordered('Джо не обладал сверх навыками но был готов дать отпор Ричарду за свой божественный напиток ',
+                   color_code_green))
     sleep(2)
     print(bordered('После этой схватки у Ричардо появился новый предмет как ПИВО,', color_code_green))
     sleep(2)
     print(bordered(
-        'это было зелье исцеление которое он мог выпить в любой следующей битве что бы восполнить своё здоровье ', color_code_green))
+        'это было зелье исцеление которое он мог выпить в любой следующей битве что бы восполнить своё здоровье ',
+        color_code_green))
     sleep(2)
     print(bordered('Позже он двинулся на север в сторону города под названием АНОР ЛОНДО.', color_code_green))
     sleep(2)
@@ -136,21 +193,26 @@ def start_history():
     sleep(2)
     print(bordered('Его следующая цель была Долина Драконов.', color_code_green))
     sleep(2)
-    print(bordered('Кузнец ему рассказал что там обитает старый вид драконов который очень опасен из за своего яда.', color_code_green))
+    print(bordered('Кузнец ему рассказал что там обитает старый вид драконов который очень опасен из за своего яда.',
+                   color_code_green))
     sleep(2)
     print(bordered('Но для ричарда его слова были лишь мотивацией пойти туда.', color_code_green))
     sleep(2)
     print(bordered(
-        'И вот спустя 3 дня непрерывной дороги он видит ущелье в котором очень странно светится ярко-зелёным цветом река и скалы.', color_code_green))
+        'И вот спустя 3 дня непрерывной дороги он видит ущелье в котором очень странно светится ярко-зелёным цветом река и скалы.',
+        color_code_green))
     sleep(2)
     print(bordered(
-        'Пройдя немного дальше он слышит громкий топот и сопение и повернув голову влево он видит его,того самого дракона про которого говорил кузнец.', color_code_green))
+        'Пройдя немного дальше он слышит громкий топот и сопение и повернув голову влево он видит его,того самого дракона про которого говорил кузнец.',
+        color_code_green))
     sleep(2)
     print(bordered(
-        'Весь покрыт плотной чешуёй тёмно-зелёного цвета дракон идёт в сторону Ричарда и Ричард понимает что битвы не избежать.', color_code_green))
+        'Весь покрыт плотной чешуёй тёмно-зелёного цвета дракон идёт в сторону Ричарда и Ричард понимает что битвы не избежать.',
+        color_code_green))
     sleep(2)
     print(bordered(
-        'И ричард делает первый рывок в его сторону и наносит своим мечом первый удар в зону живота,но из за плотной чешуи дракон не чувствует удара и атакует в ответ.', color_code_green))
+        'И ричард делает первый рывок в его сторону и наносит своим мечом первый удар в зону живота,но из за плотной чешуи дракон не чувствует удара и атакует в ответ.',
+        color_code_green))
     sleep(2)
     print(bordered('выпустив свой яд, небольшая часть одежды Ричарда расщепляется.', color_code_green))
     sleep(2)
--- main.py
+++ main.py
@@ -1,29 +1,44 @@
-from hero import Hero, start_history
+from hero import Hero, start_history, Enemy, Gost,NPC
 
 knight = Hero('Ричард', 90, 60, 75, 'меч')
 dragon = Hero('Беззубик', 200, 150, 110, 'огонь')
 gnom = Hero('Тройняшки', 30, 200, 10, 'ножик')
 kintavr = Hero('Джо', 140, 70, 65, 'бутылка пива')
-kislota_drakon = Hero('Viper', 210, 120, 110, 'кислота')
+kislota_drakon = Enemy('Viper', 210, 120, 110, 'кислота', 'ядерная слизь')
+gost = Gost('Каспер', 180, 180, 180, 'эктоплазма', 'крик')
 
-# знакомство с Ричардом
+blacksmith = NPC('Кузнец Валера', 'Болтовня')
+
+
 knight.print_info()
-start_history()
-# появление тройняшек(гномов)
-gnom.print_info()
-# битва тройняшек с Ричардом
-gnom.strike(knight)
-knight.strike(gnom)
-# знакомство с Джо(кинтавр)
-kintavr.print_info()
-# бой Джо с Ричардом
-kintavr.strike(knight)
-knight.strike(kintavr)
-# знакомство с viper
-kislota_drakon.print_info()
-# бой Ричарда с viper
-knight.strike(kislota_drakon)
-kislota_drakon.strike(knight)
+blacksmith.repair(knight)
+
+knight.print_info()
+
+# kislota_drakon.print_info()
+# kislota_drakon.apply_skill()
+#
+# gost.print_info()
+# gost.invisible()
+
+# # знакомство с Ричардом
+# knight.print_info()
+# start_history()
+# # появление тройняшек(гномов)
+# gnom.print_info()
+# # битва тройняшек с Ричардом
+# gnom.strike(knight)
+# knight.strike(gnom)
+# # знакомство с Джо(кинтавр)
+# kintavr.print_info()
+# # бой Джо с Ричардом
+# kintavr.strike(knight)
+# knight.strike(kintavr)
+# # знакомство с viper
+# kislota_drakon.print_info()
+# # бой Ричарда с viper
+# knight.strike(kislota_drakon)
+# kislota_drakon.strike(knight)
 
 '''
 
